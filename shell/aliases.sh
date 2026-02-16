76c6d045ce74dc4618e061469e349c6bdb58fb64
Ingo Karkat <swdev@ingo-karkat.de>
2026-02-16 18:54:42 +0100

    Reimplementation: Convert into my usual aichat-wrapper
    So that other extensions and external commands can easily (i.e. without exporting the aichat() function) benefit from the extensions, too.
    
    System-info: Ubuntu 24.04.4 LTS, x86_64
    Platform-info: aichat 0.30.0

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -2,8 +2,5 @@
 
 aichat()
 {
-    # Create an unnamed session by default, unless a session name is passed or
-    # the user (or the built-in completion) just queries something.
-    containsGlob '--session|-s|--list-*|--version|-V' "$@" || set -- --session temp "$@"
-    command aichat "$@"
+    aichat-wrapper "$@"
 }
