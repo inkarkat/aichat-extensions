9c977078fc3526b461aa5e7d8b429f336ddafac8
Ingo Karkat <swdev@ingo-karkat.de>
2026-02-16 19:04:35 +0100

    ENH: Add faichat alias
    Taking FILE(s) from stdin; aichat itself can only handle files passed via --file=...
    
    System-info: Ubuntu 24.04.4 LTS, x86_64
    Platform-info: aichat 0.30.0

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -4,3 +4,11 @@ aichat()
 {
     aichat-wrapper "$@"
 }
+
+# faichat	Include files and directories from standard input
+#		in the AI chat.
+faichat()
+{
+    linesToArgs --prefix --file= -- \
+	aichat-wrapper {} "$@"
+}
