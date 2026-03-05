f9bd8819b064f274726f0e1843253a4613bbc179
Ingo Karkat <swdev@ingo-karkat.de>
2026-03-05 21:23:48 +0100

    aliases: ENH: Define ai* aliases for some built-in and all custom roles
    Having to write "aichat -r ROLE" is too cumbersome.
    
    System-info: Ubuntu 24.04.4 LTS, x86_64
    Platform-info: aichat 0.30.0

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -7,3 +7,18 @@ faichat()
     linesToArgs --prefix --file= -- \
 	aichat-wrapper {} "$@"
 }
+
+# aicode	AI chat that only outputs code.
+alias aicode='aichat-wrapper --no-session --code'
+
+# aititle	AI chat that summarized into a title.
+alias aititle='aichat-wrapper --no-session --role %create-title%'
+
+# Define shortcuts for all custom roles.
+for _role in "${XDG_CONFIG_HOME:-${HOME}/.config}/aichat/roles"/*.md
+do
+    [ -r "$_role" ] || continue
+    _role="${_role##*/}"; _role="${_role%.md}"
+    alias "ai${_role}=aichat-wrapper --role $_role"
+done
+unset _role
